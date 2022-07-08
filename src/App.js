9b042e32c99dee58d3b5e953c0921add68806561
Beemhuse <brightawah94@gmail.com>
2022-07-08 10:44:17 +0100

    bright

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainLayout from './layout/main';
 import HomePage from './pages/home';
-import ServicePage from './pages/service';
-import AboutPage from './pages/about';
-import ContactPage from './pages/contact';
+import Signinpage from './pages/signin';
+import Loginpage from './pages/signin';
 import {  ThemeProvider } from '@mui/system';
 import { createTheme } from '@mui/material/styles';
 import AppTheme from './style/theme/main';
 
-// import "@fontsource/montserrat/"
 import '@fontsource/montserrat/300.css';
 import '@fontsource/montserrat/400.css';
 import '@fontsource/montserrat/500.css';
@@ -23,10 +21,10 @@ function App() {
         <Routes>
           <Route path='/' element={<MainLayout />}>
             <Route index element={<HomePage />} />
-            <Route path='service' element={<ServicePage />} />
-            <Route path='about' element={<AboutPage />} />
+            {/* <Route path='about' element={<AboutPage />} /> */}
           </Route>
-          <Route path='/contact' element={<ContactPage />} />
+            <Route path='signin' element={<Signinpage />} />
+          <Route path='/login' element={<Loginpage />} />
           {/* <Route path='*' element={<ContactPage />} /> */}
         </Routes>
       </BrowserRouter>
