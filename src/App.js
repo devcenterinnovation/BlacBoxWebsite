459d0c3209a9c9b7606cb76c3c74396fa149ac6d
Beemhuse <brightawah94@gmail.com>
2022-07-08 11:26:55 +0100

    bright

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainLayout from './layout/main';
 import HomePage from './pages/home';
-import Signinpage from './pages/signin';
+import ServicePage from './pages/service';
+import AboutPage from './pages/about';
 import Loginpage from './pages/signin';
+import Signuppage from './pages/signup';
+import ContactPage from './pages/contact';
 import {  ThemeProvider } from '@mui/system';
 import { createTheme } from '@mui/material/styles';
 import AppTheme from './style/theme/main';
 
-// import "@fontsource/montserrat/"
+
+// import "@fontsource/poppins/"
 import '@fontsource/montserrat/300.css';
 import '@fontsource/montserrat/400.css';
 import '@fontsource/montserrat/500.css';
 import '@fontsource/montserrat/700.css';
 
+// import UnitPage from './pages/unit';
+import DocumentationPage from './pages/documentation';
+
 
 
 function App() {
   return (
-    <ThemeProvider theme={createTheme(AppTheme)}>
+<ThemeProvider theme={createTheme(AppTheme)}>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<MainLayout />}>
             <Route index element={<HomePage />} />
+            <Route path='service' element={<ServicePage />} />
             <Route path='about' element={<AboutPage />} />
+          <Route path='/contact' element={<ContactPage />} />
+          <Route path='/doc' element={<DocumentationPage />} />
+          <Route path='/login' element={<Loginpage/>} />
+          <Route path='/signup' element={<Signuppage />} />
           </Route>
-            <Route path='signin' element={<Signinpage />} />
-          <Route path='/login' element={<Loginpage />} />
+          {/* <Route path='/unit' element={<UnitPage />} /> */}
           {/* <Route path='*' element={<ContactPage />} /> */}
         </Routes>
       </BrowserRouter>
 
-    </ThemeProvider>
-  );
+    </ThemeProvider>  );
 }
 
 export default App;
